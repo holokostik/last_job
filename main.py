d36fa63436ebee66ba38302b556534f54ff53c74
ilushani <114338562+ilushani@users.noreply.github.com>
2023-06-15 17:53:06 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-import time
-import telegram
+import telebot
+
+bot = telebot.TeleBot('6296708608:AAGW_e4ZSfdIjqNjc09zVW2T8cPUOW4ZVKA')
+
+
+@bot.message_handler(commands=['start'])
+def main(message):
+    bot.send_message(message.chat.id, 'Привет!')
+
+
+bot.polling(none_stop=True)
