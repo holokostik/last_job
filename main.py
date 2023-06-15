0b3acce395166d7aa3e6133b83881d7c499dca26
Константин Чекулеав <kukuruzator@mail.ru>
2023-06-15 18:05:11 +0300

    Приветственное сообщение

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import telebot
+import emoji
 
 bot = telebot.TeleBot('6296708608:AAGW_e4ZSfdIjqNjc09zVW2T8cPUOW4ZVKA')
 
 
 @bot.message_handler(commands=['start'])
 def main(message):
-    bot.send_message(message.chat.id, 'Привет!')
+    bot.send_message(message.chat.id, 'Привет! Я - бот, который может помогать тебе соблюдать распорядок дня'
+                                      'и выполнять намеченные планы. Я умею присылать напоминания о твоих делах,'
+                                      'если ты попросишь <3')
 
 
 bot.polling(none_stop=True)
